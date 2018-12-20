b274cb6266981225b95ff72437600eb16646d205
Oinary <inaho.653@gmail.com>
2018-12-21 03:08:26 +0900

    AddComment

--- DemoProject_Science/DemoProject_Science/Program.cs
+++ DemoProject_Science/DemoProject_Science/Program.cs
@@ -4,9 +4,15 @@ namespace DemoProject_Science
 {
     class Program
     {
+        static int MyID;
+        static string MyName;
+        static float MyShoesSize;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
+            MyID = 114514;
+            MyName = "高輪ゲートウェイ";
+            MyShoesSize = 26.5f;
         }
     }
 }
