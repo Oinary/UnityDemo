b0e7c75959ec59e6d61d8317d970b3a7d519f7a2
Oinary <inaho.653@gmail.com>
2018-12-22 03:03:32 +0900

    if文の例

--- DemoProject_Science/DemoProject_Science/Program.cs
+++ DemoProject_Science/DemoProject_Science/Program.cs
@@ -4,20 +4,39 @@ namespace DemoProject_Science
 {
     class Program
     {
-        static int MyID = 10;
+        static int MyPoint = 0;
 
         static void Main(string[] args)
         {
-            Console.WriteLine("一回目" + MyID);
+            MyPoint = 60;
 
-            MyID = MyID + 10;
-            Console.WriteLine("二回目" + MyID);
+            //例1
+            if(MyPoint == 50) //MyPointが50であるかどうか。
+            {
+                Console.WriteLine("ポイントは50です"); //true(正しい)
+            }
+            else //elseはfalseと判定された時に来る
+            {
+                Console.WriteLine("ポイントは50じゃありません。"); //false(正しくない)
+            }
 
-            MyID = 100 * 2;
-            Console.WriteLine("三回目" + MyID);
+            MyPoint = 180;
 
-            MyID -= 100;
-            Console.WriteLine("四回目" + MyID);
+            //例2
+            if (MyPoint > 100)//MyPointが100より大きいか。
+            {
+                Console.WriteLine("ポイントは100より多いです"); //true
+            }
+            else if (MyPoint != 0)//falseと判断された時、新たに条件分岐させる場合は else if
+            {
+                Console.WriteLine("ポイントは1以上100以下です");
+            }
         }
+
+        //実行結果
+        //
+        //例1    ポイントは50じゃありません。
+        //例2    ポイントは100より多いです
+        //
     }
 }
